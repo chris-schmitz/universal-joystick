259aceab070fab8262f7c32caa324ed4e0a8e464
chris-schmitz <schmitz.chris@gmail.com>
2017-11-15 20:40:03 -0600

    Adds in an interrupt for the button so it's not tied into the looping logic.

--- prototypes/drawing-with-joystick/drawing-with-joystick.ino
+++ prototypes/drawing-with-joystick/drawing-with-joystick.ino
@@ -31,7 +31,7 @@ Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 #define DOWN 11
 
 #define BUTTON_LED 12
-#define BUTTON 13
+#define BUTTON 2
 
 void setup () {
     Serial.begin(9600);
@@ -47,98 +47,131 @@ void setup () {
     digitalWrite(UP, HIGH);
     digitalWrite(DOWN, HIGH);
 
-    pinMode(BUTTON, INPUT);
+    // pinMode(BUTTON, INPUT);
     pinMode(BUTTON_LED, OUTPUT);
+
+    pinMode(BUTTON, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON), handleButtonPress , CHANGE);
 }
 
-int cursor [2] = {0, 0};
 
 // note that we're hard coding to the matrix grid here
 // if you abstract this to a class at some point, make the
 // grid size configurable.
-const int maxCoordinates = 64; // number of pixels in grid
+const int totalGridRows = 8;
+const int totalGridColumns = 8;
+const int maxCoordinates = totalGridRows * totalGridColumns;
 int onLEDs[maxCoordinates] = {0};
 
+int cursor [2] = {0, 0};
+bool buttonActive = false;
+bool buttonLEDActive = false;
+
+// Note that this handles two pieces of info: the button logical
+// state and the button LED state. The end goal is to fire the logical
+// branch only once per push but to handle the button LED on every loop
+// so that the button's light stays on the entire time that the user is
+// holding it down.
+void handleButtonPress () {
+    int buttonState = digitalRead(BUTTON);
+
+    // note the branch in the logic
+    if (buttonState == HIGH) {
+        buttonLEDActive = false;
+    }
+    else {
+        buttonActive = true;
+        buttonLEDActive = true;
+    }
+
+}
+
 void loop () {
+    // start fresh
+    matrix.clear();
 
-
+    // Get the state of each of the joystick directions
     int leftState = digitalRead(LEFT);
     int rightState = digitalRead(RIGHT);
     int upState = digitalRead(UP);
     int downState = digitalRead(DOWN);
 
-    int buttonState = digitalRead(BUTTON);
-
-
+    // figure out the cursor position
     // X axis
     if (rightState == LOW) {
-        cursor[0] = ++cursor[0] % 8;
+        cursor[0] = ++cursor[0] % totalGridRows;
     }
 
     if (leftState == LOW) {
         // accounting for negative 1
-        cursor[0] = ((--cursor[0] % 8) + 8) % 8;
+        cursor[0] = (--cursor[0] + totalGridRows) % totalGridRows;
     }
 
     // Y axis
     if (downState == LOW) {
-        cursor[1] = ++cursor[1] % 8;
+        cursor[1] = ++cursor[1] % totalGridColumns;
     }
 
     if (upState == LOW && cursor[1] >= 0) {
         // accounting for negative 1
-        cursor[1] = ((--cursor[1] % 8) + 8) % 8;
+        cursor[1] = (--cursor[1] + totalGridColumns) % totalGridColumns;
     }
 
-    // Serial.println("Cursor: ");
-    // Serial.println(cursor[0]);
-    // Serial.println(cursor[1]);
+    // if the button is active, handle adding or removing the pixel number
+    // currently under the cursor to the array of pixels to draw on the
+    // matrix
+    if (buttonActive == true) {
 
-    matrix.clear();
-    matrix.drawPixel(cursor[0], cursor[1], LED_GREEN);
-    // matrix.writeDisplay();
-
-    if (buttonState == LOW) {
-        Serial.println("button pressed");
-
-        int xTranslated = cursor[0] % 8;
-        int yTranslated = cursor[1] * 8;
+        int xTranslated = cursor[0] % totalGridRows;
+        int yTranslated = cursor[1] * totalGridColumns;
         int onLEDArrayPosition = xTranslated + yTranslated ;
 
-        // int onLEDArrayPosition = cursor[0] + cursor[1];
-        Serial.println("++++++++++++++++++++");
-        Serial.print("cursor x: "); Serial.println(cursor[0]);
-        Serial.print("cursor y: "); Serial.println(cursor[1]);
-        Serial.print("position: "); Serial.println(onLEDArrayPosition);
-        Serial.println("++++++++++++++++++++");
-
-        Serial.println(onLEDArrayPosition);
         togglePixelState(onLEDArrayPosition);
+
+        // once we're done with this, deactiveate the button, otherwise
+        // we end up with a blinking cursor pixel and the current pixel
+        // number being added and removed from the array with each blink.
+        // also, note that this deactivates the button _logic_ but not
+        // the button's LED.
+        buttonActive = false;
     }
 
+    // now that we've setup all of our info, update the matrix display :)
     updateMatrix();
 
+    // and delay b/c otherwise you get a fast cursor ;P
     delay(100);
 }
+
 
 void togglePixelState (int arrayPosition) {
     onLEDs[arrayPosition] = !onLEDs[arrayPosition];
 }
 
 void updateMatrix () {
+
+    // It feels a bit awkward putting this function call here b/c it's
+    // not really a matrix update, but really it would prob be awkward
+    // anywhere other than it's own function and that seems a bit like
+    // overkill (kind of like this comment block, but anyway...).
+    // The underlying purpose of this function is really just about UX,
+    // what is the feedback for the user based on their actions. That's
+    // why I put it here. maybe the function should be renamed but i'm
+    // terrible at naming things so ¯\_(ツ)_/¯
+    digitalWrite(BUTTON_LED, buttonLEDActive);
+
+    matrix.drawPixel(cursor[0], cursor[1], LED_GREEN);
+
+    // I'm sure the encoding and decoding of corridantes/pixel numbers
+    // is a bit heavy handed, but I'm still (re)learning C++ and I was
+    // having a hard time figuring out how to handle multi-dimensional
+    // arrays. This was cool to figure out anyway so I'm all good with
+    // it :)
     for (int i = 0 ; i < maxCoordinates ; i++) {
 
         if (onLEDs[i]) {
-            int row = i % 8;
-            int column = i / 8;
-            // int row = (64 -  i) % 8; // grid row size
-            // int column = i / 8; // grid column size
-
-            // Serial.println("--------------------");
-            Serial.print("index: "); Serial.println(i);
-            Serial.print("row: "); Serial.println(row);
-            Serial.print("column: "); Serial.println(column);
-            // Serial.println("--------------------");
+            int row = i % totalGridRows;
+            int column = i / totalGridColumns;
 
             if (row == cursor[0] && column == cursor[1]) {
                 matrix.drawPixel(row, column, LED_YELLOW);
